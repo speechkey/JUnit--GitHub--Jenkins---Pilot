475d1e2a836d315000fc41bf74f70edce807affc
speechkey <gremoz@gmail.com>
2012-08-08 18:05:20 +0200

    test bad

--- src/main/java/org/speechkey/junit/Largest.java
+++ src/main/java/org/speechkey/junit/Largest.java
@@ -12,7 +12,7 @@ public class Largest {
 	public static int largest(int[] list){
 		int index, max = 0;
 		for(index = 0; index < list.length-1;index++){
-			if (list[index] > max){
+			if (list[index] < max){
 				max = list[index];
 			}
 		}
